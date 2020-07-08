8bf5264475c65df8a009631930ef95dc18adae53
Raka <56627679+rakaadi@users.noreply.github.com>
2020-07-08 13:06:26 +0700

    add homepage component and routing

---
+++ src/pages/homepage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./HomePage.scss";
+import Directory from "../../components/directory/Directory.component";
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage;
