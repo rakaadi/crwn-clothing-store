f73646f594313269752114584a844708c64d5363
Raka <56627679+rakaadi@users.noreply.github.com>
2020-07-09 21:21:55 +0700

    add shoppage with collection preview and items.

---
+++ src/components/collection-preview/CollectionPreview.component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./CollectionPreview.scss"
+import CollectionItem from "../collection-item/CollectionItem.component";
+
+const CollectionPreview = ({ title, items }) => {
+    return (
+        <div className="collection-preview">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <div className="preview">
+                {
+                    items
+                        .slice(0, 4)
+                        .map(({ id, ...otherItemProps }) => (
+                            <CollectionItem key={id} {...otherItemProps} />)
+
+                        )
+                }
+            </div>
+        </div>
+    );
+}
+
+export default CollectionPreview;
