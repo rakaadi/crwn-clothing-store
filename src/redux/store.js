9a8d6396e5d0db9efb6dd981751f417b1073df74
Raka <56627679+rakaadi@users.noreply.github.com>
2020-07-16 22:08:21 +0700

    make the cart utils in cart reducer to group items when adding the same items in cart.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { createStore } from "redux";
+// import { applyMiddleware } from "redux";
+// import logger from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
-const middlewares = [logger];
+// const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+    rootReducer,
+    // applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
