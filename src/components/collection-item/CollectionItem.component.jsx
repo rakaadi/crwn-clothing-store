ba48d6f52202b8404ac9f154fee1e90779e28623
Raka <56627679+rakaadi@users.noreply.github.com>
2020-07-17 18:32:30 +0700

    added addItem and removeItem functionality to CheckoutItem component

--- src/components/collection-item/CollectionItem.component.jsx
+++ src/components/collection-item/CollectionItem.component.jsx
@@ -23,6 +23,6 @@ const CollectionItem = ({ item, addItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
     addItem: item => dispatch(addItem(item))
-})
+});
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
