d6ad5cde5cf32e934a51b1f4c08d59892955ee31
Raka <56627679+rakaadi@users.noreply.github.com>
2020-08-19 13:53:28 +0700

    Implemented redux-saga for cart and shop.

---
+++ src/redux/cart/cart.sagas.js
@@ -0,0 +1,18 @@
+import { takeLatest, all, call, put } from "redux-saga/effects";
+
+import userActionTypes from "../user/user.types";
+import { clearCart } from "./cart.actions";
+
+function* clearCartOnSignOut() {
+    yield put(clearCart());
+};
+
+export function* onSignOutSuccess() {
+    yield takeLatest(userActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
+};
+
+export function* cartSagas() {
+    yield all([
+        call(onSignOutSuccess)
+    ])
+};
